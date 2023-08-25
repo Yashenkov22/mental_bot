d532c9a6c6883443c4fefafc69f06294e8e50480
Vlad <yashenkov.v@yandex.ru>
2023-08-25 14:58:00 +0300

    add webhook mode

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+
+from aiohttp import web
 
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio import async_sessionmaker
@@ -17,35 +20,70 @@ from handlers.admin import admin_router
 from handlers.excel import excel_router
 from utils.scheduler import quiz_scheduler
 
+#For set webhook
+WEB_SERVER_HOST = '127.0.0.1'
+WEB_SERVER_PORT = 8080
 
-async def main():
-    engine = create_async_engine(db_url, echo=True)
-    async_session = async_sessionmaker(engine, expire_on_commit=False)
-     
-    bot = Bot(TOKEN)
-    dp = Dispatcher(storage=MemoryStorage())
+WEBHOOK_PATH = f'/{TOKEN}'
+BASE_WEBHOOK_URL = 'https://ffc6-178-217-121-185.ngrok.io'
 
-    dp.include_router(register_router)
-    dp.include_router(quiz_router)
-    dp.include_router(admin_router)
-    dp.include_router(excel_router)
-    dp.update.middleware(DbSessionMiddleware(session_pool=async_session))
+#Database connection
+engine = create_async_engine(db_url, echo=True)
+async_session = async_sessionmaker(engine, expire_on_commit=False)
 
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(quiz_scheduler, 'cron', second=30, args=(bot, dp.storage, async_session))
+#Initialize bot
+bot = Bot(TOKEN)
+dp = Dispatcher(storage=MemoryStorage())
 
-    # async with engine.begin() as conn:
-    #     await conn.run_sync(Base.metadata.drop_all)
-    #     await conn.run_sync(Base.metadata.create_all)
-        
-    scheduler.start()
+#Add routers to dispatcher
+dp.include_router(register_router)
+dp.include_router(quiz_router)
+dp.include_router(admin_router)
+dp.include_router(excel_router)
 
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot, engine=engine)
+#Add session and database connection in handlers 
+dp.update.middleware(DbSessionMiddleware(session_pool=async_session,
+                                         engine=engine))
+
+#Set webhook on start
+async def on_startup(bot: Bot):
+    await bot.set_webhook(f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}",
+                          drop_pending_updates=True)
+
+dp.startup.register(on_startup)
+
+#Initialize web server
+app = web.Application()
+
+#Webhook hendler
+webhook_requests_handler = SimpleRequestHandler(
+    dispatcher=dp,
+    bot=bot,
+)
+
+#Bind webhook with webserver
+webhook_requests_handler.register(app, path=WEBHOOK_PATH)
+
+setup_application(app, dp, bot=bot)
+
+#Initialize AsyncScheduler
+scheduler = AsyncIOScheduler()
+scheduler.add_job(quiz_scheduler, 'cron', second=30, args=(bot, dp.storage, async_session))
+scheduler.start()
+
+
+#Create database with start webserver
+async def start_db(app):
+    async with engine.begin() as conn:
+        # await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+app.on_startup.append(start_db)
 
 
 if __name__ == '__main__':
-    try:
-        asyncio.run(main())
-    except (KeyboardInterrupt, SystemExit):
-        print('Bot stopped')
+    event_loop = asyncio.get_event_loop()
+    web.run_app(app,
+                host=WEB_SERVER_HOST,
+                port=WEB_SERVER_PORT,
+                loop=event_loop)
--- middlewares/db.py
+++ middlewares/db.py
@@ -2,13 +2,16 @@ from typing import Callable, Awaitable, Dict, Any
 
 from aiogram import BaseMiddleware
 from aiogram.types import TelegramObject
-from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncEngine
 
 
 class DbSessionMiddleware(BaseMiddleware):
-    def __init__(self, session_pool: async_sessionmaker):
+    def __init__(self,
+                 session_pool: async_sessionmaker,
+                 engine: AsyncEngine):
         super().__init__()
         self.session_pool = session_pool
+        self.engine = engine
 
     async def __call__(
             self,
@@ -16,6 +19,10 @@ class DbSessionMiddleware(BaseMiddleware):
             event: TelegramObject,
             data: Dict[str, Any],
     ) -> Any:
+        
+        data['engine'] = self.engine
+
         async with self.session_pool() as session:
             data["session"] = session
+        
             return await handler(event, data)
