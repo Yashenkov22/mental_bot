ad1dbd1106703dde21a00a43e71c9188484f8563
Vlad <yashenkov.v@yandex.ru>
2023-08-26 12:12:49 +0300

    stable version on long polling

--- config.py
+++ config.py
@@ -9,11 +9,11 @@ load_dotenv()
 ADMIN_ID = os.environ.get('ADMIN_ID')
 TOKEN = os.environ.get('BOT_TOKEN')
 
-DB_USER = os.environ.get('POSTGRES_USER')
-DB_PASS = os.environ.get('POSTGRES_PASSWORD')
+DB_USER = os.environ.get('DB_USER')
+DB_PASS = os.environ.get('DB_PASS')
 DB_HOST = os.environ.get('POSTGRES_HOST')
-DB_PORT = os.environ.get('POSTGRES_PORT')
-DB_NAME = os.environ.get('POSTGRES_DB')
+DB_PORT = os.environ.get('DB_PORT')
+DB_NAME = os.environ.get('DB_NAME')
 
 db_url = URL.create(
         'postgresql+asyncpg',
