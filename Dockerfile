d33d774ee64ff255fa83b7d5a4135a3595f2b62d
Vlad <yashenkov.v@yandex.ru>
2023-08-31 18:58:35 +0300

    working with ngrok container and docker-compose file

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM python:3.10
 
-EXPOSE 80
-
 WORKDIR /app
 
 COPY requirements.txt requirements.txt
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import asyncio
 
+from uvicorn import Config, Server
+
+from fastapi import FastAPI, Response
+
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 
-from aiohttp import web
 
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio import async_sessionmaker
@@ -20,16 +22,21 @@ from handlers.admin import admin_router
 from handlers.excel import excel_router
 from utils.scheduler import quiz_scheduler
 
-#For set webhook
-WEB_SERVER_HOST = '127.0.0.1'
-WEB_SERVER_PORT = 8080
 
+#For set webhook
 WEBHOOK_PATH = f'/{TOKEN}'
-BASE_WEBHOOK_URL = 'https://ffc6-178-217-121-185.ngrok.io'
+BASE_WEBHOOK_URL = 'https://lenient-hippo-conversely.ngrok-free.app'
+
 
 #Database connection
 engine = create_async_engine(db_url, echo=True)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+#Create database with start webserver
+async def start_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 #Initialize bot
 bot = Bot(TOKEN)
@@ -45,26 +52,34 @@ dp.include_router(excel_router)
 dp.update.middleware(DbSessionMiddleware(session_pool=async_session,
                                          engine=engine))
 
+#Initialize web server
+app = FastAPI()
+event_loop = asyncio.get_event_loop()
+config = Config(app=app,
+                loop=event_loop,
+                host='0.0.0.0',
+                port=8000)
+server = Server(config)
+
 #Set webhook on start
-async def on_startup(bot: Bot):
+@app.on_event('startup')
+async def on_startup():
     await bot.set_webhook(f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}",
                           drop_pending_updates=True)
+    await start_db()
 
-dp.startup.register(on_startup)
 
-#Initialize web server
-app = web.Application()
+@app.get(WEBHOOK_PATH)
+async def any():
+    return {'status': 'ok'}
 
-#Webhook hendler
-webhook_requests_handler = SimpleRequestHandler(
-    dispatcher=dp,
-    bot=bot,
-)
+@app.post(WEBHOOK_PATH)
+async def bot_webhook(update: dict):
+    tg_update = types.Update(**update)
+    await dp.feed_update(bot=bot, update=tg_update)
 
-#Bind webhook with webserver
-webhook_requests_handler.register(app, path=WEBHOOK_PATH)
 
-setup_application(app, dp, bot=bot)
+
 
 #Initialize AsyncScheduler
 scheduler = AsyncIOScheduler()
@@ -72,18 +87,7 @@ scheduler.add_job(quiz_scheduler, 'cron', second=30, args=(bot, dp.storage, asyn
 scheduler.start()
 
 
-#Create database with start webserver
-async def start_db(app):
-    async with engine.begin() as conn:
-        # await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
-
-app.on_startup.append(start_db)
 
 
 if __name__ == '__main__':
-    event_loop = asyncio.get_event_loop()
-    web.run_app(app,
-                host=WEB_SERVER_HOST,
-                port=WEB_SERVER_PORT,
-                loop=event_loop)
+    event_loop.run_until_complete(server.serve())
